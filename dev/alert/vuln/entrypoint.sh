82479f4bbd403823a407ab0431fd96c464f20f52
adiffpirate <35786917+adiffpirate@users.noreply.github.com>
2024-02-19 04:43:51 -0300

    Improved code readbility by adding guard clauses

--- dev/alert/vuln/entrypoint.sh
+++ dev/alert/vuln/entrypoint.sh
@@ -14,31 +14,31 @@ while true; do
 		}
 	}" | yq -P '.data.results | .[]')
 
-	if [ -n "$vuln" ]; then
-		echo "$vuln" | notify -silent -bulk -provider-config <(yq -n -P '{
-			"telegram":[{
-				"id": "vuln",
-				"telegram_api_key": env(TELEGRAM_API_KEY),
-				"telegram_chat_id": env(TELEGRAM_CHAT_ID),
-				"telegram_format": "```yaml\n{{data}}\n```",
-				"telegram_parsemode": "Markdown"
-			}]
-		}')
-
-		$UTILS/query_dgraph.sh -q $query_file "
-			mutation {
-				addVuln(input: [{
-					\"name\": \"$(echo "$vuln" | yq -r '.["Vuln.name"]')\",
-					\"notified\": true
-				}], upsert: true){
-					vuln { name, notified }
-				}
-			}
-		" | jq -c .
-	else
+	if [ -z "$vuln" ]; then
 		echo "Nothing to alert. Trying again in $ALERT_COOLDOWN_SECONDS seconds"
+		sleep $ALERT_COOLDOWN_SECONDS
+		continue
 	fi
 
-	sleep $ALERT_COOLDOWN_SECONDS
+	echo "$vuln" | notify -silent -bulk -provider-config <(yq -n -P '{
+		"telegram":[{
+			"id": "vuln",
+			"telegram_api_key": env(TELEGRAM_API_KEY),
+			"telegram_chat_id": env(TELEGRAM_CHAT_ID),
+			"telegram_format": "```yaml\n{{data}}\n```",
+			"telegram_parsemode": "Markdown"
+		}]
+	}')
+
+	$UTILS/query_dgraph.sh -q $query_file "
+		mutation {
+			addVuln(input: [{
+				\"name\": \"$(echo "$vuln" | yq -r '.["Vuln.name"]')\",
+				\"notified\": true
+			}], upsert: true){
+				vuln { name, notified }
+			}
+		}
+	" | jq -c .
 
 done
