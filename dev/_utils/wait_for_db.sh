d4c3c251bb50fb30b584c2688b2d3019feb30351
adiffpirate <35786917+adiffpirate@users.noreply.github.com>
2024-02-19 19:57:10 -0300

    Avoid traping errors from wait_for_db

--- dev/_utils/wait_for_db.sh
+++ dev/_utils/wait_for_db.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-script_path=$(dirname "$0")
-
-set -eEo pipefail
-trap '$script_path/_stacktrace.sh "$?" "$BASH_SOURCE" "$BASH_COMMAND" "$LINENO"' ERR
-
 until curl --no-progress-meter --fail $DGRAPH_ALPHA_HOST:$DGRAPH_ALPHA_HTTP_PORT/health > /dev/null; do
 	>&2 echo 'Waiting Alpha Server to be ready'
 	sleep 1
