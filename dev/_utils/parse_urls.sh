d8ef5171137c311dd741de9dd623c9b58a0aa012
adiffpirate <adiffpirate@gmail.com>
2026-02-13 13:12:02 -0300

    add parse_domain function

--- dev/_utils/parse_urls.sh
+++ dev/_utils/parse_urls.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
-
 script_path=$(dirname "$0")
-
 set -eEo pipefail
 trap '>&2 $script_path/_stacktrace.sh "$?" "$BASH_SOURCE" "$BASH_COMMAND" "$LINENO"' ERR
+
+parse_domain(){
+	domain="$1"
+	subdomain="$2"
+
+	# Count the number of words delimeted by a dot
+	domain_level="$(echo "$domain" | awk -F. '{print NF}')"
+
+	# Get the parent domain (e.g. for "foo.example.com" the parent_domain is "example.com")
+	parent_domain="$(echo "$domain" | sed -E 's|^([^\.]+\.)(.+)$|\2|')"
+
+	# Determine domain type (tld, root or sub)
+	if grep -qw "$domain" $script_path/tld-list.txt; then # If domain is TLD
+		domain_type='tld'
+	elif grep -qw "$parent_domain" $script_path/tld-list.txt; then # If domain is one level above TLD
+		domain_type='root'
+	else # If domain is two or more levels above TLD
+		domain_type='sub'
+	fi
+
+	# Print parsed JSON (with subdomain if provided)
+	jq -nc "{
+		record_type: \"Domain\",
+		record_data: {
+			name: \"$domain\",
+			type: \"$domain_type\",
+			level: \"$domain_level\",
+			$(if [[ -n "$subdomain" ]]; then echo "subdomains:[{name: \"$subdomain\"}],"; fi)
+			randomSeed: \"$(uuidgen -r)\"
+		}
+	}"
+
+	# Call this function recursively for the parent domains until it reachs TLD
+	if [[ "$domain_type" != "tld" ]]; then
+		parse_domain "$parent_domain" "$domain"
+	fi
+}
 
 # Read input from stdin
 if [[ -t 0 ]]; then
@@ -34,4 +69,8 @@ for url in $URLS_LIST; do
 		$script_path/_log.sh 'warn' 'URL is missing the domain' "protocol=$protocol" "domain=$domain" "path=$path" "args=$args"
 		continue
 	fi
+
+	$script_path/_log.sh 'debug' 'Parsing URL' "protocol=$protocol" "domain=$domain" "path=$path" "args=$args"
+	parse_domain "$domain"
+
 done
